2ddb47b594da6d7ec2ed9496bea0777b677fb9d3
Zaliya Validova <z.validova@yandex.ru>
2022-01-31 16:18:32 +0500

    обновленное усложненное задание

--- script.js
+++ script.js
@@ -21,14 +21,23 @@ switch (lang) {
         console.log("Повторите ввод");
 }
 
+// 1 вариант - преобразовать булевое значение в число и по нему обратиться к массиву
 let arrWeek = [
     ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"],
     ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"]
 ];
-
 let a = lang === "ru";
 a = Number(a);
-console.log(arrWeek[a]);
+console.log("Вариант 1: " + arrWeek[a]);
+
+// 2 вариант - обратиться по ключу массива
+
+let week = new Map ([
+    ['en', ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"]],
+    ['ru', ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"]],
+]);
+console.log("Вариант 2: "+ week.get(lang));
+
 
 // Задача 2
 
