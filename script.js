5d0f92366135c6d62878d2af3a546764708bfc9e
Zaliya Validova <z.validova@yandex.ru>
2022-02-05 20:50:07 +0500

    обновление 7 усложненного

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
 const week = [
 "Понедельник",
 "Вторник",
@@ -11,13 +13,20 @@ const week = [
 let day = new Date(); // текущий день
 let todayIndex = day.getDate();
 
-for (let i = 0; i< week.length; i++) {
-    console.log(week[i]);
-} 
-
-console.log("Сегодня " + week[todayIndex]);
-
-// for (let i = 0; i< week.length; i++) {  Пыталась добавить через document
-//     let a = week[i];
-//     document.write(a);
+// for (let i = 0; i< week.length; i++) {
+//     console.log(week[i].bold());
 // } 
+
+for (let i = 0; i< week.length; i++) { 
+    let a = week[i];
+    if ((i === 5 || i === 6) && i !==todayIndex ) {
+        document.write("<p>" + a.italics() + "</p>");
+    } else if ((i !== 5 || i !== 6) && i === todayIndex) {
+        document.write("<p>" + a.bold() + "</p>"); 
+    } else if ((i === 5 || i === 6) && i === todayIndex) {
+        document.write("<p>" + a.bold().italics() + "</p>"); 
+    } else {
+        document.write("<p>" + a + "</p>");
+    }
+} 
+});
