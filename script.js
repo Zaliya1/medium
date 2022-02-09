34053cc305a521bc3fdfbfb4c975e0fbb46703c4
Zaliya Validova <z.validova@yandex.ru>
2022-02-09 18:20:52 +0500

    обновление 9 дз

--- script.js
+++ script.js
@@ -2,77 +2,36 @@
 const time = function() {
 
 let now = new Date();
-const indexDay = now.getDay();
-const indexMonth = now.getMonth();
-console.log(indexMonth);
+const arrWeek = ["понедельник","вторник","среда", "четверг", "пятница", "суббота", "воскресенье"];
+const day = arrWeek[now.getDay()];
+const arrMonth = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"];
+const month = arrMonth[now.getMonth()];
 const date = now.getDate();
 const year = now.getFullYear();
-const hour = now.getHours();
+const hour = now.getHours();  
 const minute = now.getMinutes();
 const second = now.getSeconds();
 
-const month = function(index) {
-    const arrMonth= {
-        0: "января",
-        1: "февраля",
-        2: "марта",
-        3: "апреля",
-        4: "мая",
-        5: "июня",
-        6: "июля",
-        7: "августа",
-        8: "сентября",
-        9: "октября",
-        10: "ноября",
-        11: "декабря",
-    };
-    return arrMonth[index];
-};
-const dayWeek = function(index) {
-    const arrWeek= {
-        0: "понедельник",
-        1: "вторник",
-        2: "среда",
-        3: "четверг",
-        4: "пятница",
-        5: "суббота",
-        6: "воскресенье",
-    };
-    return arrWeek[index];
-};
+function declOfNum(n, arr) {  
+    n = Math.abs(n) % 100; 
+    let n1 = n % 10;
+    if (n > 10 && n < 20) { 
+        return arr[2]; 
+    } else {
+        if ((n1 >= 5 && n1 < 20) || n1 === 0) { return arr[2]; }
+        if (n1 > 1 && n1 < 5) { return arr[1]; }
+        if (n1 == 1) { return arr[0]; }
+    }
+}
+declOfNum(11, ['час', 'часа', 'часов']); // вернёт — минута
+declOfNum(17, ['минута', 'минуты', 'минут']); // вернёт — минута
+declOfNum(53, ['секунда', 'секунды', 'секунд']); // вернёт — минута
 
-function checkHour (num) {
-    if (num === 1) {
-        num = num + " час";
-    } else if(num > 1 && num <5) {
-        num = num + " часа";
-    } else if (num >=5) {
-        num = num + " часов";
-    }
-    return num;
-}
-function checkMinute (num) {
-    if (num === 1) {
-        num = num + " минута";
-    } else if(num > 1 && num <5) {
-        num = num + " минуты";
-    } else if (num >=5) {
-        num = num + " минут";
-    }
-    return num;
-}
-function checkSecond (num) {
-    if (num === 1) {
-        num = num + " секунда";
-    } else if(num > 1 && num <5) {
-        num = num + " секунды";
-    } else if (num >=5) {
-        num = num + " секунд";
-    }
-    return num;
-}
+const hour1 = declOfNum(hour, ['час', 'часа', 'часов']);
+const minute1 = declOfNum(minute, ['минута', 'минуты', 'минут']);
+const second1 = declOfNum(second, ['секунда', 'секунды', 'секунд']);
 
-const todayVersionA = "Сегодня " + dayWeek(indexDay) + ", " + date + " " + month(indexMonth) + " " + year + " года, " + checkHour(hour) + " " + checkMinute(minute) +" " + checkSecond(second);
+const todayVersionA = "Сегодня " + day + ", " + date + " " + month + " " + year + " года, " + hour + " " + hour1 + " " + minute + " " + minute1 + " " + second + " " + second1;
 
 const todayVersionB = new Date().toLocaleString(); 
 
